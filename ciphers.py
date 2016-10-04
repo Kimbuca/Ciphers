69225fc844c9c67250ad12d987255da96d1e6010
Kimbuca <a01225884@itesm.mx>
2016-10-04 16:20:15 -0500

    Update ciphers.py

--- ciphers.py
+++ ciphers.py
@@ -2,7 +2,7 @@
 abc = ['A', 'B', 'C', 'D', 'E', 'F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 msg = []
 
-def getLeftShiftedArray(shift):
+def getShiftedArray(shift):
 	temp = list(abc)
 	final = temp[shift:] + temp[:shift]
 	return final
@@ -10,8 +10,8 @@ def getLeftShiftedArray(shift):
 
 def encryptCaesar(shift, text):
 	del msg[:]
-	alph = getLeftShiftedArray(shift%26)
-	for i in range(0, len(text)):
+	alph = getShiftedArray(shift%26)
+	for i in range(0, len(text)):	
 		if(text[i] != ' '):
 			msg.append(alph[ord(str.upper(text[i])) - 65])
 			continue;
@@ -19,9 +19,10 @@ def encryptCaesar(shift, text):
 	return ''.join(msg)
 
 
+
 def decryptCaesar(shift, text):
 	del msg[:]
-	alph = getLeftShiftedArray(26 - (shift%26))
+	alph = getShiftedArray(26 - (shift%26))
 	for i in range(0, len(text)):
 		if(text[i] != ' '):
 			msg.append(alph[ord(str.upper(text[i])) - 65])
@@ -31,18 +32,22 @@ def decryptCaesar(shift, text):
 
 
 def encryptVigenere(key, text):
+
 	del msg[:]
 	for i in range(0, len(text)):
 		shift = (ord(text[i])- 65)
-		row = getLeftShiftedArray(shift)
+		row = getShiftedArray(shift)
 		msg.append(row[ord(key[i % len(key)]) - 65])
 	return ''.join(msg)
 
-
 def decryptVigenere(key, enc_text):	
+
 	del msg[:]
 	for i in range(0, len(enc_text)):
 		shift = (ord(key[i % len(key)]) - 65)
-		row = getLeftShiftedArray(26 - shift)
+		row = getShiftedArray(26 - shift)
 		msg.append(row[(ord(enc_text[i]) - 65)])
 	return ''.join(msg)
+
+print(encryptVigenere("FORTIFICATION", "DEFENDTHEEASTWALLOFTHECASTLE"))
+
